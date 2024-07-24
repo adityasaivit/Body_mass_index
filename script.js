d9ec413218e347e9e497c119c9478e1d265d60df
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-07-24 21:27:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
-let bmi=0;
-function calc()
+function result()
 {
-    var w=document.querySelector("#weight").value;
-    let h=document.querySelector("#height").value;
+    let w=Number(document.querySelector(".wei").value)
+    let h=Number(document.querySelector(".hei").value)
+    let bmi=w/(h*h);
 
-    bmi=w/(h*h);
-    bmi=bmi.toFixed(2);
-    document.querySelector(".bmi").innerText=bmi;
-    bmi=Number(bmi);
-    console.log(typeof(bmi<18))
-    if(bmi<18)
-    {
-        document.querySelector(".res").innerText="Bro! You are too thin";
-        document.querySelector(".res").style.color="red";
-    }
-    else if(bmi>18 && bmi<28)
-    {
-        document.querySelector(".res").innerText="Bro! you are ok now";
-        document.querySelector(".res").style.color="rgb(255, 217, 0)";
-
-    }
-    else{
-        document.querySelector(".res").innerText="Bro! you are overweight";
-        document.querySelector(".res").style.color="red";
-    }
+    document.querySelector(".res-disp").innerText=bmi.toFixed(2);
 }
-
